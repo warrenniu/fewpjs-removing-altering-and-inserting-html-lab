9362c06b11c516b8eb26d379b35474af8020e60f
warrenniu <warrenniu@gmail.com>
2020-11-16 19:59:27 -0500

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+// Write your code here
+main.remove()
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion"
+body.append(newHeader)
